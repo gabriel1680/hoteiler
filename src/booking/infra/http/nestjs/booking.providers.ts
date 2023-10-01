2a435e81585dab6e71c2daad71bebc48003bd465
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-10-01 18:48:25 -0300

    feat: add nestjs providers

--- src/booking/infra/http/nestjs/booking.providers.ts
+++ src/booking/infra/http/nestjs/booking.providers.ts
@@ -1,3 +1,34 @@
 import { Provider } from "@nestjs/common";
+import { DataSource } from "typeorm";
 
-export const bookingProviders: Provider[] = [];
+import { EventBus } from "src/@kernel/application/EventBus";
+import { TransactionalSession } from "src/@kernel/application/TransactionalSession";
+import { HotelFacade } from "src/admin/facade/HotelFacade";
+import { BookRoom } from "src/booking/application/usecase/BookRoom";
+import { BookRepository } from "src/booking/domain/BookRepository";
+import { TypeORMBookRepository } from "../../database/typeorm/TypeORMBookRepository";
+import { BookEntity } from "../../database/typeorm/entities/BookEntity";
+
+export const bookingProviders: Provider[] = [
+	{
+		provide: "BookRepository",
+		useFactory: (connection: DataSource) =>
+			new TypeORMBookRepository(connection.getRepository(BookEntity)),
+		inject: ["DataSource"],
+	},
+	{
+		provide: BookRoom,
+		useFactory: (
+			session: TransactionalSession,
+			repository: BookRepository,
+			hotelService: HotelFacade,
+			eventBus: EventBus
+		) => new BookRoom(session, repository, hotelService, eventBus),
+		inject: [
+			"TransactionalSession",
+			"BookRepository",
+			"HotelFacade",
+			"EventBus",
+		],
+	},
+];
--- src/main/http/nestjs/app.module.ts
+++ src/main/http/nestjs/app.module.ts
@@ -1,6 +1,35 @@
 import { Module } from "@nestjs/common";
+import { DataSource } from "typeorm";
+import amqp from "amqplib";
+
 import { AdminModule } from "src/admin/infra/http/nestjs/admin.module";
 import { BookingModule } from "src/booking/infra/http/nestjs/booking.module";
 
-@Module({ imports: [AdminModule, BookingModule] })
+@Module({
+	imports: [AdminModule, BookingModule],
+	providers: [
+		{
+			provide: "EventBus",
+			useFactory: async () => {
+				return await amqp.connect("amqp://localhost");
+			},
+		},
+		{
+			provide: "DataSource",
+			useFactory: async () => {
+				const ds = new DataSource({
+					type: "sqlite",
+					database: ":memory:",
+					synchronize: true,
+					entities: [
+						__dirname + "/../../../**/entities/**/*.{js,ts}",
+					],
+					logging: ["error"],
+				});
+				await ds.initialize();
+				return ds;
+			},
+		},
+	],
+})
 export class AppModule {}
