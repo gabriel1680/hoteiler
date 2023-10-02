9397c17b5965047a6b64caff9edac17ed0f0263e
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-10-02 20:47:17 -0300

    feat(booking): add rest api

--- src/booking/infra/http/nestjs/booking.controller.ts
+++ src/booking/infra/http/nestjs/booking.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+
+import { BookRoom } from "src/booking/application/usecase/BookRoom";
+import { ListBook } from "src/booking/application/usecase/ListBook";
 
 @Controller("/book")
-export class BookingController {}
+export class BookingController {
+    constructor(
+        private readonly bookRoom: BookRoom,
+        private readonly listBook: ListBook
+    ) {}
+
+    @Post("/:hotelId")
+    create(@Param("hotelId") hotelId: string, @Body() dto: any) {
+        return this.bookRoom.execute({
+            hotelId,
+            endDate: dto.end_date,
+            startDate: dto.start_date,
+            roomNumber: dto.room_number,
+        });
+    }
+
+    @Get("/:hotelId")
+    get(@Param("hotelId") hotelId: string) {
+        return this.listBook.execute({ hotelId });
+    }
+}
